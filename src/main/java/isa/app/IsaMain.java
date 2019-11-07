a14dadb375ebb8c1e096d993df8bf56ea0f107e9
unknown <stefanmicic8697@gmail.com>
2019-11-07 09:58:17 +0100

    Inicijalizovan projekat

---
+++ src/main/java/isa/app/IsaMain.java
@@ -0,0 +1,13 @@
+package isa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaMain {
+		
+	public static void main(String[] args){
+		SpringApplication.run(IsaMain.class,args);
+		
+	}
+}
