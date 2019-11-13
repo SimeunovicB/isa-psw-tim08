ba7ea8a53e4c89866a54762382fe98faaf74cb69
Mraseta <marko.raseta97@gmail.com>
2019-11-13 23:51:21 +0100

    added functionality for changing

--- psw-isa-front/src/app/profile/profile.component.ts
+++ psw-isa-front/src/app/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormsModule, EmailValidator } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { CookieService } from 'ngx-cookie-service';
+import { jsonpFactory } from '@angular/http/src/http_module';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  firstName:string;
+  lastName : string;
+  address :string;
+  password:string;
+  l :any;
+  
+
+  constructor(private http: Http,
+    private router: Router,
+    private cookieService: CookieService,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.l=JSON.parse(this.cookieService.get('loggedUser'));
+    console.log(this.l);
+    this.firstName=this.l.firstName;
+    this.lastName=this.l.lastName;
+    this.address=this.l.address;
+  }
+  onSubmit(form: NgForm) {
+   
+    this.userService.UpdateUser(this.firstName,this.lastName,this.address,this.l.id)
+    .subscribe(
+      (user: any) => {
+        this.router.navigate(['/']);
+      }, (error) => alert(error.text)
+    );
+  }
+  onSubmitPassword(form: NgForm) {
+   
+    this.userService.UpdateUserPassword(this.password,this.l.id)
+    .subscribe(
+      (user: any) => {
+        this.router.navigate(['/']);
+      }, (error) => alert(error.text)
+    );
   }
 
 }
--- psw-isa-front/src/app/services/user.service.ts
+++ psw-isa-front/src/app/services/user.service.ts
@@ -31,6 +31,71 @@ export class UserService {
       })
     )
   }
+  RegUser( 
+    firstname:string,
+    lastname : string,
+    address :string,
+    jmbg : string,
+    email:string,
+    password : string) {
+    return this.http.post("http://localhost:9090/api/patient/register", {
+      firstName:firstname,
+      lastName:lastname,
+      address:address,
+      jmbg:jmbg,
+      email: email,
+      password: password
+    })
+    .pipe(
+      map((response: Response) => {
+        //console.log(response);
+        //this.cookieService.set('loggedUser', JSON.stringify(response.json()));
+        const data = response.json();
+        return data;
+      }),
+      catchError((err: Response) => {
+        return throwError(JSON.parse(err.text()));
+      })
+    )
+  }
+  UpdateUserPassword( 
+    password :string,
+    id:number) {
+    return this.http.post("http://localhost:9090/api/person/changePassword", {
+      password:password,
+      id:id
+    })
+    .pipe(
+      map((response: Response) => {
+        const data = response.json();
+        return data;
+      }),
+      catchError((err: Response) => {
+        return throwError(JSON.parse(err.text()));
+      })
+    )
+  }
+  UpdateUser( 
+    firstname:string,
+    lastname : string,
+    address :string,
+    id:number) {
+    return this.http.put("http://localhost:9090/api/person/update", {
+      firstName:firstname,
+      lastName:lastname,
+      address:address,
+      id:id
+    })
+    .pipe(
+      map((response: Response) => {
+        const data = response.json();
+        return data;
+      }),
+      catchError((err: Response) => {
+        return throwError(JSON.parse(err.text()));
+      })
+    )
+  }
 
   getPending() {
     return this.http.get("http://localhost:9090/api/patient")
