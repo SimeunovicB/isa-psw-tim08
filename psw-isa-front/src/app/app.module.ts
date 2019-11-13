63c6fad834a2aa96362ae6687c3690e867daf95a
unknown <stefanmicic8697@gmail.com>
2019-11-13 20:30:29 +0100

    front improved

--- psw-isa-front/src/app/app.module.ts
+++ psw-isa-front/src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { UserService } from './services/user.service';
+import { HttpModule } from '@angular/http';
+import { CookieService } from 'ngx-cookie-service';
+
+const appRoutes: Routes = [
+  { path: '', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    CookieService,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- psw-isa-front/src/app/login/login.component.ts
+++ psw-isa-front/src/app/login/login.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
+import { Http } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from '../services/user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,
+    private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.userService.postUser(this.email, this.password)
+    .subscribe(
+      (user: any) => {
+        this.cookieService.set('loggedUser', JSON.stringify(user));
+        this.router.navigate(['/']);
+      }, (error) => alert(error.text)
+    );
   }
 
 }
