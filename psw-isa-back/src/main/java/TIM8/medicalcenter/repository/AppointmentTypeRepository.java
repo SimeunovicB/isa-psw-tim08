b39d6981659452563e0ea3a27038ed08680458ab
Mraseta <marko.raseta97@gmail.com>
2019-12-15 19:16:50 +0100

    minor changes

--- psw-isa-back/src/main/java/TIM8/medicalcenter/repository/AppointmentTypeRepository.java
+++ psw-isa-back/src/main/java/TIM8/medicalcenter/repository/AppointmentTypeRepository.java
@@ -2,6 +2,10 @@ package TIM8.medicalcenter.repository;
 
 import TIM8.medicalcenter.model.AppointmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,4 +13,10 @@ public interface AppointmentTypeRepository extends JpaRepository<AppointmentType
 
     List<AppointmentType> findAll();
     AppointmentType findByName(String name);
+    AppointmentType findOneById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query("update AppointmentType at set at.name = :name where c.id = :id")
+    int updateAppointmentType(@Param("name") String name, @Param("id") Long id);
 }
--- psw-isa-back/src/main/resources/data-postgres.sql
+++ psw-isa-back/src/main/resources/data-postgres.sql
@@ -2,7 +2,7 @@
 
 -- noinspection SqlDialectInspectionForFile
 
-insert into clinic(name,address,description) values('clinic1','dr klinike 1','very clininac clinic hoho');
+insert into clinic(name,address,description) values('clinic1','dr klinike 1','clinic desc');
 
 --passwordi su 123
 insert into person(type,first_name,last_name,username,password,address,status,jmbg,enabled)
