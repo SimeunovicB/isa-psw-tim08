36ed8f6b7bf63b0680915ca8246a47dad6340951
Mraseta <marko.raseta97@gmail.com>
2020-02-09 01:29:40 +0100

    minor changes

--- psw-isa-front/src/Interceptors/TokenInterceptor.ts
+++ psw-isa-front/src/Interceptors/TokenInterceptor.ts
@@ -7,13 +7,11 @@ import {
 } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs/Observable';
-import { JwtHelperService } from '@auth0/angular-jwt'
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   constructor(private cookieService: CookieService) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let helper = new JwtHelperService();
     if (this.cookieService.get('token') !== null) {
       request = request.clone({
         setHeaders: {
